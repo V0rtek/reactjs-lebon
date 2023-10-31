193ad7206ff88991ed4659f7629e0d249bf52b6e
2266308 <2266308@cstjeanc.qc.ca>
2023-10-31 11:13:02 -0400

    comm1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import SearchBar from './SearchBar.js';
+// Checké le tutoriel github du prof.
+
+/**
+ * .
+ * @return {HTML}
+ */
+function App() {
+  return (
+    <div>
+      <h1>Filterable React List</h1>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SearchBar.js
@@ -0,0 +1,13 @@
+/**
+ * .
+ * @return {HTML}
+ */
+function SearchBar() {
+  return (
+    <form>
+      <input type="text" placeholder="Search"></input>
+    </form>
+  );
+}
+
+export default SearchBar;
